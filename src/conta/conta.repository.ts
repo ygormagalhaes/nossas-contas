f189a8c536e3446fb03f6c2947ece40f48991a3a
Ygor <emaildoygor@gmail.com>
2019-05-15 11:26:13 -0300

    Mudança para utilização de AbstractRepository ao invés de Repository para encapsular os métodos de contato direto com a base de dados em cada repositório das entidades

--- src/conta/conta.repository.ts
+++ src/conta/conta.repository.ts
@@ -1,11 +1,11 @@
-import { Repository, EntityRepository } from 'typeorm';
+import { EntityRepository, AbstractRepository } from 'typeorm';
 import { Conta } from './conta.model';
 
 @EntityRepository(Conta)
-export class ContaRepository extends Repository<Conta> {
+export class ContaRepository extends AbstractRepository<Conta> {
 
     async listarContas() {
-        const contas = await super.find();
+        const contas = await super.repository.find();
         return contas;
     }
 }
