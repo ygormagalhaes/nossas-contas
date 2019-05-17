d270c0d7738f6c21f6fb684e5ce25e11a9ac139d
Ygor <emaildoygor@gmail.com>
2019-05-17 14:18:02 -0300

    Criação da entidade usuário

--- src/usuario/usuario.model.ts
+++ src/usuario/usuario.model.ts
@@ -1,3 +1,27 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity({
+    name: 'Usuarios',
+})
 export class Usuario {
-// TODO: Implementar model
+
+    @PrimaryGeneratedColumn({
+        name: 'ID',
+    })
+    id: number;
+
+    @Column({
+        name: 'Email',
+        type: 'varchar',
+        length: 255,
+    })
+    email: string;
+
+    @Column({
+        name: 'Senha',
+        type: 'varchar',
+        length: 8,
+    })
+    senha: string; // TODO: Guardar senha criptografada.
+
 }
