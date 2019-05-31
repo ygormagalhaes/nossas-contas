b3b46b5c157d4f66ad4b9c4344efe306bce23ac6
Ygor <emaildoygor@gmail.com>
2019-05-31 10:29:51 -0300

    WIP

--- src/pipes/parse-usuario.pipe.ts
+++ src/pipes/parse-usuario.pipe.ts
@@ -1,15 +1,14 @@
-import { PipeTransform, ArgumentMetadata, BadRequestException } from '@nestjs/common';
+import { PipeTransform, ArgumentMetadata } from '@nestjs/common';
 import * as Joi from '@hapi/joi';
 import { Usuario } from '../usuario/usuario.model';
 import { usuarioSchema } from '../joi-schemas/usuario.schema';
-import { UsuarioException } from '../usuario/usuario.exception';
 
 export class ParseUsuarioPipe implements PipeTransform<any, any> {
 
     transform(value: any, metadata: ArgumentMetadata): Usuario {
         const { error } = Joi.validate(value, usuarioSchema);
         if (error) {
-            // throw error;
+            throw error;
         }
         return value;
     }
