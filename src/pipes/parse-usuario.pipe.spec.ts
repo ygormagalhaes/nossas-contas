0b93409f09fb1b3ef64a4d3eb8b935b897019f2b
Ygor <emaildoygor@gmail.com>
2019-05-31 11:13:08 -0300

    Testes para ParseUsuarioPipe

--- src/pipes/parse-usuario.pipe.spec.ts
+++ src/pipes/parse-usuario.pipe.spec.ts
@@ -1,7 +1,6 @@
 import { UsuarioPayload } from './../interfaces/usuario-payload.interface';
 import { ParseUsuarioPipe } from './parse-usuario.pipe';
 import { UsuarioException } from '../usuario/usuario.exception';
-import { BadRequestException } from '@nestjs/common';
 
 describe('ParseUsuarioPipe', () => {
     let payload: UsuarioPayload;
@@ -16,7 +15,7 @@ describe('ParseUsuarioPipe', () => {
     it('deve lançar um erro caso o payload seja nulo', () => {
         expect(() => {
             new ParseUsuarioPipe().transform(undefined, undefined);
-        }).toThrowError();
+        }).toThrow(new UsuarioException(UsuarioException.DADOS_NULOS));
     });
 
     it('deve lançar um erro caso o payload tenha um email nulo', () => {
@@ -26,38 +25,37 @@ describe('ParseUsuarioPipe', () => {
         }).toThrow(new UsuarioException(UsuarioException.EMAIL_INVALIDO));
     });
 
-    xit('deve lançar um erro caso o payload tenha um email inválido', () => {
+    it('deve lançar um erro caso o payload tenha um email inválido', () => {
         payload.email = 'foobar';
         expect(() => {
             new ParseUsuarioPipe().transform(payload, undefined);
-        }).toThrow(new BadRequestException(UsuarioException.EMAIL_INVALIDO));
+        }).toThrow(new UsuarioException(UsuarioException.EMAIL_INVALIDO));
     });
 
-    xit('deve lançar um erro caso a senha seja nula', () => {
+    it('deve lançar um erro caso a senha seja nula', () => {
         delete payload.senha;
         expect(() => {
             new ParseUsuarioPipe().transform(payload, undefined);
-        }).toThrow(new BadRequestException(UsuarioException.SENHA_INVALIDA));
+        }).toThrow(new UsuarioException(UsuarioException.SENHA_INVALIDA));
     });
 
-    xit('deve lançar um erro caso a senha tenha menos que 6 caracteres', () => {
+    it('deve lançar um erro caso a senha tenha menos que 6 caracteres', () => {
         payload.senha = '123';
         expect(() => {
             new ParseUsuarioPipe().transform(payload, undefined);
-        }).toThrow(new BadRequestException(UsuarioException.SENHA_INVALIDA));
+        }).toThrow(new UsuarioException(UsuarioException.SENHA_INVALIDA));
     });
 
-    xit('deve lançar um erro caso a senha tenha mais que 8 caracteres', () => {
+    it('deve lançar um erro caso a senha tenha mais que 8 caracteres', () => {
         payload.senha = 'asfasfias0fi0is';
         expect(() => {
             new ParseUsuarioPipe().transform(payload, undefined);
-        }).toThrow(new BadRequestException(UsuarioException.SENHA_INVALIDA));
+        }).toThrow(new UsuarioException(UsuarioException.SENHA_INVALIDA));
     });
 
-    xit('não deve lançar nenhum erro com um payload válido', () => {
+    it('não deve lançar nenhum erro com um payload válido', () => {
         expect(() => {
             new ParseUsuarioPipe().transform(payload, undefined);
         }).not.toThrowError();
     });
-
 });
--- src/pipes/parse-usuario.pipe.ts
+++ src/pipes/parse-usuario.pipe.ts
@@ -1,11 +1,17 @@
-import { PipeTransform, ArgumentMetadata } from '@nestjs/common';
 import * as Joi from '@hapi/joi';
+import { PipeTransform, ArgumentMetadata } from '@nestjs/common';
+import { UsuarioException } from './../usuario/usuario.exception';
 import { Usuario } from '../usuario/usuario.model';
 import { usuarioSchema } from '../joi-schemas/usuario.schema';
+import { ObjectUtils } from '../utils/object.utils';
 
 export class ParseUsuarioPipe implements PipeTransform<any, any> {
 
     transform(value: any, metadata: ArgumentMetadata): Usuario {
+        if (ObjectUtils.isObjectEmpty(value)) {
+            throw new UsuarioException(UsuarioException.DADOS_NULOS);
+        }
+
         const { error } = Joi.validate(value, usuarioSchema);
         if (error) {
             throw error;
