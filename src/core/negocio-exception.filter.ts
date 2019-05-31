e4f61aa468530a5343031430d1cc78e452ebcd3a
Ygor <emaildoygor@gmail.com>
2019-05-31 09:59:25 -0300

    Ajuste no filter de exceptions de negócio para lançar uma BadRequestException

--- src/core/negocio-exception.filter.ts
+++ src/core/negocio-exception.filter.ts
@@ -1,21 +1,9 @@
-import { ExceptionFilter, Catch, ArgumentsHost, HttpStatus } from '@nestjs/common';
+import { ExceptionFilter, Catch, ArgumentsHost, BadRequestException } from '@nestjs/common';
 import { NegocioException } from './negocio-exception';
 
 @Catch(NegocioException)
 export class NegocioExceptionFilter implements ExceptionFilter {
-
     catch(exception: NegocioException, host: ArgumentsHost) {
-        const context = host.switchToHttp();
-        const response = context.getResponse();
-
-        console.error(exception);
-
-        response
-            .status(HttpStatus.BAD_REQUEST)
-            .json({
-                statusCode: HttpStatus.BAD_REQUEST,
-                message: exception.message,
-            });
+        throw new BadRequestException(exception.message);
     }
-
 }
