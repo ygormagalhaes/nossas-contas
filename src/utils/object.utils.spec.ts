270ce04975226d36e8897a2d8e60b98d52ad84c6
Ygor <emaildoygor@gmail.com>
2019-05-31 11:09:44 -0300

    Testes para classe ObjectUtils

--- src/utils/object.utils.spec.ts
+++ src/utils/object.utils.spec.ts
@@ -1,3 +1,19 @@
-describe('ObjectUtils', () => {
+import { ObjectUtils } from './object.utils';
 
+describe('ObjectUtils', () => {
+    it('deve retornar false caso um objeto válido seja testado', () => {
+        expect(ObjectUtils.isObjectEmpty({ foo: 1 })).toBeFalsy();
+    });
+
+    it('deve retornar true caso undefined seja testado', () => {
+        expect(ObjectUtils.isObjectEmpty(undefined)).toBeTruthy();
+    });
+
+    it('deve retornar true caso null seja testado', () => {
+        expect(ObjectUtils.isObjectEmpty(null)).toBeTruthy();
+    });
+
+    it('deve retornar true caso {} seja testado', () => {
+        expect(ObjectUtils.isObjectEmpty({})).toBeTruthy();
+    });
 });
