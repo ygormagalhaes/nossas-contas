9ef5df2178182240077ee1e70f5ae106b964efcc
Ygor <emaildoygor@gmail.com>
2019-06-10 18:28:28 -0300

    WIP

--- src/joi-schemas/cartao.schema.ts
+++ src/joi-schemas/cartao.schema.ts
@@ -4,6 +4,7 @@ import * as Joi from '@hapi/joi';
 export const cartaoSchema = Joi.object().keys({
     descricao:
         Joi.string()
+            .min(4)
             .max(255)
             .required()
             .error(new ContaException(ContaException.CARTAO_DESCRICAO)),
