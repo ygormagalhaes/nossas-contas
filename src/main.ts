e435f0ca056a56c4b1b234622b3401b5c8301c6e
Ygor <emaildoygor@gmail.com>
2019-06-11 18:29:43 -0300

    Configuração inicial do Swagger na aplicação

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NegocioExceptionFilter } from './core/negocio-exception.filter';
-import { AuthGuard } from '@nestjs/passport';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.useGlobalFilters(new NegocioExceptionFilter());
+
+    const options = new DocumentBuilder()
+        .setTitle('Financeirinho')
+        .setDescription('Documentação da API Financeirinho')
+        .setVersion('1.0')
+        .build();
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('api', app, document);
+
     await app.listen(3000);
 }
 bootstrap();
