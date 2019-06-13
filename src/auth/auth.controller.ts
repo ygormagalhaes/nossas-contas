9835a39f226d944b727e66b54af18922bb296b13
Ygor <emaildoygor@gmail.com>
2019-06-13 16:38:14 -0300

    WIP

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,16 @@
+import { AuthException } from './auth.exception';
 import { LoginDTO } from './login.dto';
 import { Controller, Post, Body } from '@nestjs/common';
 import { UsuarioPipe } from '../pipes/usuario.pipe';
 import { AuthService } from './auth.service';
+import { ApiBadRequestResponse } from '@nestjs/swagger';
 
 @Controller('session')
 export class AuthController {
 
     constructor(private readonly authService: AuthService) { }
 
+    @ApiBadRequestResponse({ description: AuthException.CREDENCIAIS_INVALIDAS })
     @Post('login')
     async login(@Body(new UsuarioPipe()) payload: LoginDTO) {
         return await this.authService.login(payload);
