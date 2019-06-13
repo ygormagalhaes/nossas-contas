b02ed76bfe35293b251c57cc56ee84d9c92efaa3
Ygor <emaildoygor@gmail.com>
2019-06-13 16:03:48 -0300

    Renomeando endpoints de login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,13 +3,15 @@ import { UsuarioPipe } from '../pipes/usuario.pipe';
 import { UsuarioPayload } from './../interfaces/usuario-payload.interface';
 import { AuthService } from './auth.service';
 
-@Controller('auth')
+@Controller('session')
 export class AuthController {
 
     constructor(private readonly authService: AuthService) { }
 
-    @Post('')
+    @Post('login')
     async login(@Body(new UsuarioPipe()) payload: UsuarioPayload) {
         return await this.authService.login(payload);
     }
+
+    // TODO: Implementar logout
 }
